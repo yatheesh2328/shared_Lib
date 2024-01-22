be1b1c5e334f0e0d77f0cc2a819aa04e80a6284d
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-01-22 19:52:00 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -2,7 +2,7 @@
 
    def call() {
        // Your SonarQube scan steps go here
-       sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://18.170.23.153:9000'
+       sh 'mvn sonar:sonar -Dsonar.projectKey=sonar -Dsonar.host.url=http://18.170.23.153:9000 -Dsonar.java.binaries=target/classes'
    }
 
    return this
