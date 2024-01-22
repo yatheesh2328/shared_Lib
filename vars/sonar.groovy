92465d391fc88fb153ffdfc204433c4e1998d4e7
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-01-22 16:49:13 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,8 +1,11 @@
-def call(Map config) {
-    // Set default values if not provided
-    def sonarHostUrl = config.sonarHostUrl ?: 'http://localhost:9000'
-    def projectKey = config.projectKey ?: 'my-project'
-    def sourcesPath = config.sourcesPath ?: 'src'
-    def sonarPropertiesPath = "${JENKINS_HOME}/workspace/${JOB_NAME}/sonarqube.properties"
-    sh "mvn sonar:sonar -Dsonar.config.file=${sonarPropertiesPath}"
-}  
+def runSonarQubeAnalysis() {
+    // Configure SonarQube properties
+    def sonarQubeScannerHome = tool 'SonarQubeScanner'
+    def projectKey = '24a4b741c1e41323d590cf08bad6bc3c0361da73'
+    def projectName = 'bus-booking'
+    def sonarQubeServerUrl = 'http://13.40.155.22:9000'
+
+        -Dsonar.projectKey=${24a4b741c1e41323d590cf08bad6bc3c0361da73} \
+        -Dsonar.projectName=${bus-bookin} \
+        -Dsonar.host.url=${http://13.40.155.22:9000}", 
+        returnStatus: true
