b80fb8a82ef3a3bd8d22ffaa1e433bc16e4a2bc6
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-01-22 16:37:02 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,6 +1,8 @@
-def call(){
-    echo "Running Sonar Analysis & ${name}"
-   sh '''
-    mvn clean package sonar:sonar
-    '''
-}
+def call(Map config) {
+    // Set default values if not provided
+    def sonarHostUrl = config.sonarHostUrl ?: 'http://localhost:9000'
+    def projectKey = config.projectKey ?: 'my-project'
+    def sourcesPath = config.sourcesPath ?: 'src'
+    def sonarPropertiesPath = "${JENKINS_HOME}/workspace/${JOB_NAME}/sonarqube.properties"
+    sh "mvn sonar:sonar -Dsonar.config.file=${sonarPropertiesPath}"
+}    
